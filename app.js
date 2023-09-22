8fa6b9383b703d7fb859fe9bc4fe25e4801bd524
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-09-21 23:58:29 -0300

    configuraçao do cors

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const guestsRoutes = require('./routes/guestsRoutes');
 const giftsRoutes = require('./routes/giftsRoutes');
+
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
