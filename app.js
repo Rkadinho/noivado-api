16e3c711703a9c3b205fdc58a1c1bd3738a9960d
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 02:53:19 -0300

    teste

--- app.js
+++ app.js
@@ -21,5 +21,10 @@ app.use('/guests', guestsRoutes);
 app.use('/gifts', giftsRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta http://rkadinho.github.io/noivado-api`);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
+
+var Parse = require('parse/node');
+
+Parse.initialize("pZE5WRPLSk2FG7xzbdzMxur4G6pv0FKwqWgVSwTX","W7cE7zGTBVYX8RS8V0cEhCA8CZv9dXiSEAVFeVGO");
+Parse.serverURL = 'https://parseapi.back4app.com/'
