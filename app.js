219c9a2432306089eb5144a5163ad15d39d143bc
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 16:17:54 -0300

    ajuste no back end

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const URL_PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
 const URL_ORIGIN = process.env.ORIGIN
@@ -9,7 +9,7 @@ const guestsRoutes = require('./routes/guestsRoutes');
 const giftsRoutes = require('./routes/giftsRoutes');
 
 const corsOptions = {
-  origin: 'https://noivado-front.vercel.app',
+  origin: URL_ORIGIN,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
@@ -21,3 +21,7 @@ app.use(express.json());
 
 app.use('/guests', guestsRoutes);
 app.use('/gifts', giftsRoutes);
+
+app.listen(URL_PORT, () => {
+  console.log(`Servidor rodando na porta ${URL_PORT}`);
+});
