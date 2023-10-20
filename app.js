d7a339d58fdcd28c01fc5fd0b7f24db93b0c38a8
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 18:23:10 -0300

    teste do server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const URL_PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
 const URL_ORIGIN = process.env.ORIGIN
@@ -20,3 +21,7 @@ app.use(express.json());
 
 app.use('/guests', guestsRoutes);
 app.use('/gifts', giftsRoutes);
+
+app.listen(URL_PORT, () => {
+  console.log(`Servidor rodando na porta ${URL_PORT}`);
+});
