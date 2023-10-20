63bf6d66418ac953966a754cca6a3d5449d39c4f
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 10:36:46 -0300

    ajuste no .env

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const guestsRoutes = require('./routes/guestsRoutes');
 const giftsRoutes = require('./routes/giftsRoutes');
 
 const corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: env("ORIGIN"),
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
@@ -23,8 +23,3 @@ app.use('/gifts', giftsRoutes);
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
-
-var Parse = require('parse/node');
-
-Parse.initialize("pZE5WRPLSk2FG7xzbdzMxur4G6pv0FKwqWgVSwTX","W7cE7zGTBVYX8RS8V0cEhCA8CZv9dXiSEAVFeVGO");
-Parse.serverURL = 'https://parseapi.back4app.com/'
