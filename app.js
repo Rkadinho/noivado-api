82b7bd9b00aca3e4b166af4305f1246dcebe0bf8
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 21:36:29 -0300

    middleware do cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const URL_PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const URL_ORIGIN = process.env.ORIGIN
 
@@ -21,7 +21,3 @@ app.use(express.json());
 
 app.use('/guests', guestsRoutes);
 app.use('/gifts', giftsRoutes);
-
-app.listen(URL_PORT, () => {
-  console.log(`Servidor rodando na porta ${URL_PORT}`);
-});
