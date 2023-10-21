f05d85a1d2116dd7eabfe628d7466fb9452d5895
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-20 21:22:06 -0300

    middleware do cors

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const URL_ORIGIN = process.env.ORIGIN
 const guestsRoutes = require('./routes/guestsRoutes');
 const giftsRoutes = require('./routes/giftsRoutes');
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
+const customCorsMiddleware = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', URL_ORIGIN || '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Options-Success', '204');
+  next();
 };
 
-app.use(cors(corsOptions));
+app.use(customCorsMiddleware);
 
 app.use(express.json());
 
