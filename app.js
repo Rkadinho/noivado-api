a6462ed53acb31ee3a0221900f7b47b2d03c44f7
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-21 13:28:38 -0300

    ajuste na config do env

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config();
 
 const URL_PORT = process.env.PORT || 3000
-const URL_ORIGIN = process.env.ORIGIN || '*'
+const URL_ORIGIN = process.env.ORIGIN
 
 const guestsRoutes = require('./routes/guestsRoutes')
 const giftsRoutes = require('./routes/giftsRoutes')
@@ -11,6 +12,7 @@ const giftsRoutes = require('./routes/giftsRoutes')
 const corsOptions = {
   origin: URL_ORIGIN
 }
+
 
 app.use(cors(corsOptions))
 
